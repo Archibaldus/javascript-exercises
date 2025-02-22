99fa04b8c1ef67bb731aba1430735f0f0c06ece4
Arthur <arthur.lukiewicz@gmail.com>
2025-02-22 06:53:00 +0100

    exercise 2: added repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(repeatedString, repeatCount) {
+    let result = '';
+    if (repeatCount < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < repeatCount; i++) {
+        result += repeatedString;
+    }
+    return result;
 };
 
 // Do not edit below this line
